86e78a8a602dd8f538da6a3eaeafbfe2b2a55e6c
wbudd <wbudd@users.noreply.github.com>
2019-06-06 10:40:47 +0900

    Fixed mistakenly auto-renamed <ringsocket_app_helper.h>

--- src/ringsocket_app.h
+++ src/ringsocket_app.h
@@ -68,7 +68,7 @@ struct rs {
     uint16_t src_worker_thread_i;
 };
 
-#include <rs_app_helper.h>
+#include <ringsocket_app_helper.h>
 
 #define _RS_APP(init_macro, open_macro, read_macro, close_macro, \
     timer_macro) \
