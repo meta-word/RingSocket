f50b7712ad7effb9cd853db7c360d62695988dbe
Will Budd <wbudd@users.noreply.github.com>
2020-05-07 10:55:38 +0900

    Improve stress app recipient-selection algorithm

--- tests/rst_app_stress.c
+++ tests/rst_app_stress.c
@@ -2,6 +2,7 @@
 // Copyright © 2019 William Budd
 
 #include <ringsocket.h>
+#include <stdio.h> // sprintf
 
 #define RST_MAX_CLIENT_C 1000
 #define RST_MAX_SIMUL_MSG_PER_CLIENT_C 5
@@ -11,8 +12,7 @@
 typedef enum {
     RST_FATAL = -1,
     RST_OK = 0,
-    RST_TOO_MANY_CLIENTS = 4000,
-    RST_BAD_MSG = 4001
+    RST_TOO_MANY_CLIENTS = 4000
 } rst_ret;
 
 struct rst_client {
@@ -26,6 +26,7 @@ struct rst_stress {
     struct rst_client clients[RST_MAX_CLIENT_C];
     uint64_t total_msg_byte_c;
     int client_c;
+    int avail_client_c;
     unsigned max_msg_content_size;
 };
 
@@ -44,34 +45,25 @@ static struct rst_client * get_client(
     return NULL;
 }
 
-static void remove_client(
-    struct rst_stress * s,
-    struct rst_client * client
-) {
-    struct rst_client * last_client = s->clients + --s->client_c;
-    if (client < last_client) {
-        memmove(client, client + 1, sizeof(*client) * (last_client - client));
-    }
-    memset(last_client, 0, sizeof(*last_client));
-}
-
 static int randrange( // Crappy randomness is OK for the purpose of this app
     int range
 ) {
     return (intmax_t) range * rand() / ((intmax_t) RAND_MAX + 1);
 }
 
-static void shuffle_clients( // Perform a Fisher-Yates shuffle over all clients
-    struct rst_stress * s
+// Perform a Fisher-Yates shuffle over an array of clients
+static void shuffle_clients(
+    struct rst_client * clients,
+    int client_c
 ) {
-    if (s->client_c < 2) {
+    if (client_c < 2) {
         return;
     }
-    for (int i = s->client_c - 1; i; i--) {
+    for (int i = client_c - 1; i; i--) {
         int j = randrange(i + 1);
-        struct rst_client c = s->clients[j];
-        s->clients[j] = s->clients[i];
-        s->clients[i] = c;
+        struct rst_client c = clients[j];
+        clients[j] = clients[i];
+        clients[i] = c;
     }
 }
 
@@ -95,46 +87,48 @@ static void send_anything_anywhere(
     if (cur_client->msg_c > RST_MAX_SIMUL_MSG_PER_CLIENT_C / 2) {
         return;
     }
-    shuffle_clients(s);
-    static uint64_t ids[RST_MAX_CLIENT_C] = {0};
-    static char ports_str[RST_MAX_CLIENT_C * RS_CONST_STRLEN("12345, ")] = {0};
-    bool include_cur = s->total_msg_byte_c % 2;
-    int recipient_c = 0;
-    {
-        int ready_c = 0;
-        char * p = ports_str;
-        for (struct rst_client * c = s->clients; c < s->clients + s->client_c;
-            c++) {
-            if (c->msg_c >= RST_MAX_SIMUL_MSG_PER_CLIENT_C ||
-                (c == cur_client && !include_cur)) {
-                continue;
-            }
-            ids[ready_c++] = c->id;
-            int x = c->port;
-            int y = x % 10000;
-            *p = '0' + x / 10000; p += x != y; x = y; y = x % 1000;
-            *p = '0' + x /  1000; p += x != y; x = y; y = x % 100;
-            *p = '0' + x /   100; p += x != y; x = y; y = x % 10;
-            *p = '0' + x /    10; p += x != y;
-            *p++ = '0' + y; *p++ = ','; *p++ = ' ';
-        }
-        p[-2] = '\0';
-        recipient_c = randrange(ready_c) + 1;
-    }
+
+    // Any clients preceding avail_clients have reached their
+    // RST_MAX_SIMUL_MSG_PER_CLIENT_C and therefore are currently not available.
+    // (Design-wise, unavailable clients must be kept at the head rather than
+    // the tail, because newly connected clients are added to the tail, and they
+    // start out as being available.)
+    struct rst_client * avail_clients =
+        s->clients + s->client_c - s->avail_client_c;
+    shuffle_clients(avail_clients, s->avail_client_c);
+
+    bool include_cur = (s->total_msg_byte_c + 1) % 2;
+    int recipient_c = randrange(s->avail_client_c) + include_cur;
     if (!recipient_c) {
         return;
     }
 
     uint64_t msg_id = generate_random_msg_id();
     rs_w_uint64_hton(rs, msg_id);
+
+    thread_local static uint64_t ids[RST_MAX_CLIENT_C] = {0};
+    thread_local static char ports_str[
+        RST_MAX_CLIENT_C * RS_CONST_STRLEN("12345, ")] = {0};
     {
-        struct rst_client * c = s->clients;
-        for (int i = 0; i < recipient_c; i++, c++) {
-            while (c->msg_c >= RST_MAX_SIMUL_MSG_PER_CLIENT_C) {
-                c++;
-            }
+        uint64_t * id = ids;
+        char * p_str = ports_str;
+        for (struct rst_client * c = avail_clients,
+            * const c_over = avail_clients + recipient_c; c < c_over; c++) {
+            *id++ = c->id;
+            p_str += sprintf(p_str, "%d, ", c->port);
             c->msg_ids[c->msg_c++] = msg_id;
+            if (c->msg_c == RST_MAX_SIMUL_MSG_PER_CLIENT_C) {
+                // From here on out this client will no longer be available
+                // until its msg_c decreases, so ensure that it no longer will
+                // be among the avail_clients by swapping its contents with
+                // avail_clients[0] and then incrementing avail_clients.
+                struct rst_client c_swap = *c;
+                *c = *avail_clients;
+                *avail_clients++ = c_swap;
+                s->avail_client_c--;
+            }
         }
+        p_str[-2] = '\0';
     }
 
     int content_size = RS_MAX(
@@ -154,7 +148,7 @@ static void send_anything_anywhere(
     }
     s->total_msg_byte_c += recipient_c * (16 + content_size);
     
-    if (recipient_c > s->client_c / 2) {
+    if (recipient_c > (s->client_c + 1) / 2) {
         if (recipient_c == s->client_c) {
             RS_LOG(LOG_DEBUG, "Sending 16+%zu bytes to_every[%s]",
                 content_size, ports_str);
@@ -227,6 +221,7 @@ rst_ret open_cb(
     );
     RS_LOG(LOG_DEBUG, "Connection established with new client on remote port "
         "%d: client_c is now %" PRIu32, client->port, s->client_c);
+    s->avail_client_c++;
     send_anything_anywhere(rs, s, client);
     return RST_OK;
 }
@@ -249,31 +244,31 @@ rst_ret read_cb(
             while (++i < client->msg_c) {
                 client->msg_ids[i - 1] = client->msg_ids[i];
             }
-            client->msg_ids[--client->msg_c] = 0;
+            if (client->msg_c-- == RST_MAX_SIMUL_MSG_PER_CLIENT_C) {
+                s->avail_client_c++;
+            }
+            client->msg_ids[client->msg_c] = 0;
             goto size_validation;
         }
     }
     RS_LOG(LOG_ERR, "Received a message from remote port %d with an ID of %"
         PRIu64 " not included in its array of size %d of pending verifications",
         client->port, msg_id, client->msg_c);
-        remove_client(s, client);
-    return RST_BAD_MSG;
+    return RST_FATAL;
 
     size_validation:
     if (declared_size != content_size) {
-        RS_LOG(LOG_ERR, "received a message from remote port %d with a "
+        RS_LOG(LOG_ERR, "Received a message from remote port %d with a "
             "declared content size of %zu despite its actual content size of "
             "%zu", client->port, declared_size, content_size);
-        remove_client(s, client);
-        return RST_BAD_MSG;
+        return RST_FATAL;
     }
     for (size_t i = 0; i < content_size; i++) {
         if (content[i] != 255 - i % 256) { // See send_anything_anywhere()
             RS_LOG(LOG_ERR, "Received from remote port %d a content byte at "
                 "index %zu with a value of %" PRIu8 " instead of the expected "
                 "value %" PRIu8, client->port, i, content[i], 255 - i % 256);
-            remove_client(s, client);
-            return RST_BAD_MSG;
+            return RST_FATAL;
         }
     }
     s->total_msg_byte_c -= 16 + content_size;
@@ -287,19 +282,10 @@ rst_ret read_cb(
 rst_ret close_cb(
     rs_t * rs
 ) {
-    struct rst_stress * s = rs_get_app_data(rs);
-    struct rst_client * client = get_client(rs, s);
-    if (!client) {
-        RS_LOG(LOG_CRIT, "get_client() failed unexpectedly. Shutting down...");
-        return RST_FATAL;
-    }
-    if (client->msg_c) {
-        RS_LOG(LOG_WARNING, "Close callback called for prematurely for remote "
-            "port %d despite having %d pending message validations remaining. ",
-            client->port, client->msg_c);
-    }
-    remove_client(s, client);
-    return RST_OK;
+    (void) rs;
+    RS_LOG(LOG_CRIT, "The stress app expects to be the side doing any "
+        "connection closing, not the other way around!");
+    return RST_FATAL;
 }
 
 RS_APP(
